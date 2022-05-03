2ada09bc4ed65b19652041e0a125e1fb71272e2b
Bob Witmer <bwitmer@golden.net>
2022-05-03 14:55:37 -0400

    Chapter 2.3 Exercise 2

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel1: UILabel!
+    @IBOutlet weak var messageLabel2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabel1.text = ""
+        messageLabel2.text = ""
+        print("viewDidLoad()")
     }
 
-
+    @IBAction func labelButton1(_ sender: Any) {
+        print("Button 1 Pressed")
+        messageLabel1.text = "Larry Page"
+        messageLabel2.text = "Sergey Brin"
+    }
+    @IBAction func labelButton2(_ sender: Any) {
+        print("Button 2 Pressed")
+        messageLabel1.text = "Jennifer Hyman"
+        messageLabel2.text = "Jenny Fleiss"
+    }
+    @IBAction func labelButton3(_ sender: Any) {
+        print("Button 3 Pressed")
+        messageLabel1.text = ""
+        messageLabel2.text = ""
+    }
+    
 }
 
